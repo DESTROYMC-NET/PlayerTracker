97afd00fc3b3ad30e40d88113367e641e0f5bb8d
hyperdefined <contact@hyper.lol>
2021-07-09 12:09:01 -0400

    Update CommandPlayer.java

--- src/main/java/lol/hyper/playertracker/commands/CommandPlayer.java
+++ src/main/java/lol/hyper/playertracker/commands/CommandPlayer.java
@@ -24,7 +24,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
@@ -49,60 +48,27 @@ public class CommandPlayer implements CommandExecutor {
         int argsLength = args.length;
         if (argsLength == 1) {
             UUID uuid = Bukkit.getOfflinePlayer(args[0]).getUniqueId();
-            if (playerTracker.usingMYSQL) {
-                try {
-                    if (playerTracker.mysqlController.lookUpFirstJoin(uuid) == null) {
-                        sender.sendMessage(ChatColor.RED + "Player was not found. Maybe they changed their username?");
-                        return true;
-                    } else {
-                        lastPlayed = Date.from(Instant.ofEpochMilli(
-                                Long.parseLong(playerTracker.mysqlController.lookUpLastLogin(uuid))));
-                        lastPlayedString = simpleDateFormat.format(lastPlayed);
-                        joinDate = Date.from(Instant.ofEpochMilli(
-                                Long.parseLong(playerTracker.mysqlController.lookUpFirstJoin(uuid))));
-                        joinDateString = simpleDateFormat.format(joinDate);
-                        sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
-                        sender.sendMessage(
-                                ChatColor.DARK_AQUA + args[0] + " was first seen on " + joinDateString + " EST.");
-                        if (Bukkit.getServer().getPlayerExact(args[0]) != null && !PlayerTracker.isVanished(args[0])) {
-                            sender.sendMessage(ChatColor.DARK_AQUA + args[0] + " is currently online.\n");
-                        } else {
-                            sender.sendMessage(
-                                    ChatColor.DARK_AQUA + args[0] + " was last seen on " + lastPlayedString + " EST.");
-                        }
-                        sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
-                    }
-                    return true;
-                } catch (SQLException e) {
-                    sender.sendMessage(ChatColor.RED + "There was an error retrieving player data.");
-                    e.printStackTrace();
-                    playerTracker.mysqlController.connect();
-                    return true;
-                }
-            } else {
-                if (!playerTracker.jsonController.doesPlayerExist(uuid)) {
-                    sender.sendMessage(ChatColor.RED + "Player was not found. Maybe they changed their username?");
-                    return true;
-                } else {
-                    lastPlayed = Date.from(
-                            Instant.ofEpochMilli(Long.parseLong(playerTracker.jsonController.getLastLogin(uuid))));
-                    lastPlayedString = simpleDateFormat.format(lastPlayed);
-                    joinDate = Date.from(
-                            Instant.ofEpochMilli(Long.parseLong(playerTracker.jsonController.getFirstJoin(uuid))));
-                    joinDateString = simpleDateFormat.format(joinDate);
-                    sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
-                    sender.sendMessage(
-                            ChatColor.DARK_AQUA + args[0] + " was first seen on " + joinDateString + " EST.");
-                    if (Bukkit.getServer().getPlayerExact(args[0]) != null && !PlayerTracker.isVanished(args[0])) {
-                        sender.sendMessage(ChatColor.DARK_AQUA + args[0] + " is currently online.\n");
-                    } else {
-                        sender.sendMessage(
-                                ChatColor.DARK_AQUA + args[0] + " was last seen on " + lastPlayedString + " EST.");
-                    }
-                    sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
-                }
+            if (!playerTracker.jsonController.doesPlayerExist(uuid)) {
+                sender.sendMessage(ChatColor.RED + "Player was not found. Maybe they changed their username?");
                 return true;
+            } else {
+                lastPlayed = Date.from(
+                        Instant.ofEpochMilli(Long.parseLong(playerTracker.jsonController.getLastLogin(uuid))));
+                lastPlayedString = simpleDateFormat.format(lastPlayed);
+                joinDate = Date.from(
+                        Instant.ofEpochMilli(Long.parseLong(playerTracker.jsonController.getFirstJoin(uuid))));
+                joinDateString = simpleDateFormat.format(joinDate);
+                sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
+                sender.sendMessage(ChatColor.DARK_AQUA + args[0] + " was first seen on " + joinDateString + " EST.");
+                if (Bukkit.getServer().getPlayerExact(args[0]) != null && !PlayerTracker.isVanished(args[0])) {
+                    sender.sendMessage(ChatColor.DARK_AQUA + args[0] + " is currently online.\n");
+                } else {
+                    sender.sendMessage(
+                            ChatColor.DARK_AQUA + args[0] + " was last seen on " + lastPlayedString + " EST.");
+                }
+                sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
             }
+            return true;
         }
         sender.sendMessage(ChatColor.RED + "Invalid option. Usage: /player <player> to find player info.");
         return true;
